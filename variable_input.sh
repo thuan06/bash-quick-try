9c787b4f16394753cc57aa54415c8b2e60b09a99
Dương Đình Thuận <thuandd@appota.com>
2017-07-14 23:56:46 +0700

    [2017-07-14] Variable continue

--- variable_input.sh
+++ variable_input.sh
@@ -20,7 +20,7 @@ echo "process id:" $$
 echo "user:" $USER
 echo "hostname:" $HOSTNAME
 echo "line number:" $LINENO
-
+echo "DATE:" $date
 
 echo"****************************************************"
 name=orion
@@ -28,3 +28,21 @@ echo "Name:" $name" la toi"
 
 longname="This is a big sale off"
 echo $longname
+
+echo"****************************************************"
+echo "Check difference between '' and ' in script"
+today="Black Friday"
+echo $today
+tomorrow1='next $today'
+echo 'tomorrow single quote: '$tomorrow1
+tomorrow2="next $today"
+echo "tomorrow double quote" $tomorrow2
+
+echo "****************************************************"
+echo "command subtitution: dua ket qua cua command --> varianble"
+lineInEtc=$( ls -al /etc | wc -l )
+echo "Number of file and directory in etc " $lineInEtc
+
+ls /root/Documents
+listCurrent=$(ls /root/Documents)
+echo "print current file and folder" $listCurrent
